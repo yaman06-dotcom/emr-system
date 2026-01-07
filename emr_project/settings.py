cf9d36767bf1706bb4d84d7ca49876dd655c2778
yaman06-dotcom <yamanbanibaker2233@gmail.com>
2026-01-07 16:21:29 +0300

    Update settings.py

--- emr_project/settings.py
+++ emr_project/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
